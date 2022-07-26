4ad3c0cebb20d01ff3263c65ddc2edc9e6d0c248
Zubair <36886070+emZubair@users.noreply.github.com>
2022-07-27 00:55:15 +0500

    Pylint Testing (#10)

--- dj_mako/urls.py
+++ dj_mako/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('assessment.urls')),
+
     path('admin/', admin.site.urls),
-    path('', include('assessment.urls'))
 ]
